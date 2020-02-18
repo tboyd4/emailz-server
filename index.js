0c17155dbb2eab45a9316014d954364f3004180f
tboyd <tboyd1898@gmail.com>
2020-02-18 15:34:46 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// imports the express library, and assigns it to variable express
+const express = require('express');
+
+// creating express application (there can be multiple of these within a project)
+const app = express();
+
+// creating first route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+})
+
+// telling express what port to listen to (either a dynamic port from Heroku, or 5000 on our local machine)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
