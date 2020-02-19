5bc72b3a67d70290ae7820d7a0304b096c2ba959
tboyd <tboyd1898@gmail.com>
2020-02-19 10:15:17 -0600

    added google oauth to project

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // imports the express library, and assigns it to variable express
 const express = require('express');
 
+// importing 3rd party libraries
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 // creating express application (there can be multiple of these within a project)
 const app = express();
 
-// creating first route handler
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-})
+// telling passport.js to understand and use the GoogleStrategy class, which will recieve some info it will need to use
+passport.use(new GoogleStrategy());
+
+
+
+// route handlers
+
+
+
 
 // telling express what port to listen to (either a dynamic port from Heroku, or 5000 on our local machine)
 const PORT = process.env.PORT || 5000;
