b4946cbcdef1e1af087de7ac494de418672af985
tboyd <tboyd1898@gmail.com>
2020-02-19 13:40:12 -0600

    finished first stage of google oauth and installed nodemon

--- index.js
+++ index.js
@@ -5,16 +5,32 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+// importing keys file from congi
+const keys = require('./config/keys');
+
 // creating express application (there can be multiple of these within a project)
 const app = express();
 
 // telling passport.js to understand and use the GoogleStrategy class, which will recieve some info it will need to use
-passport.use(new GoogleStrategy());
-
-
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {                   // GoogleStrategy takes two arguements 1.) objects 2.) callback function
+    console.log('access token : ' + accessToken);
+    console.log('refresh token : ' + refreshToken);   
+    console.log('profile : ' + profile);                     
+})
+);
 
 // route handlers
+    // route handles google auth login requests
+app.get('/auth/google', passport.authenticate('google', {       // google string is interally known by passport as GoogleStrategy
+    scope: ['profile', 'email']                                 // Object with scope specifies what information we are needing to get profile and email are internally known by google as things
+})
+);
 
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 
