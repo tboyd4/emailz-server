63fe3b754f6362659c32be69d46e35b6f83f3e94
tboyd <tboyd1898@gmail.com>
2020-02-24 14:32:03 -0600

    added react app to client directory within server directory. installed concurrently to run backend express and react servers at the same time

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component {
+  
+
+
+  render() {
+
+
+    return (
+      <div className="App">
+        <h1>This is a Test</h1>
+      </div>
+    );
+
+  }
+}
+
+export default App;
