9fd39fdda376109dab93a1b36b319f61c48590e4
tboyd <tboyd1898@gmail.com>
2020-02-25 13:00:44 -0600

    added auth reducer and reducer directory in the client

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux'; // This provider component will wrap the App component, and will force react and redux to talk to each other nicely
+import { createStore, applyMiddleware } from 'redux';
+
+
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(()=>[], {}, applyMiddleware()); // creating a redux store
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector("#root"));
