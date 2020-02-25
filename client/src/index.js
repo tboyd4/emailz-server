adfdfe455642021fc33071e12bf3f5f26aad0f2c
tboyd <tboyd1898@gmail.com>
2020-02-25 12:27:56 -0600

    added test output to app component

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App /> , )
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
