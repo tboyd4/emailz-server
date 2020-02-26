6f0a1b30ad907d5e29b4025d44fd056742a5bd51
tboyd <tboyd1898@gmail.com>
2020-02-26 16:09:24 -0600

    wiring up redux thunk

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'; // This provider component will wrap the App component, and will force react and redux to talk to each other nicely
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers'; // importing all reducers (that were combined) in my reducers directory
 
 import App from "./components/App";
 
-const store = createStore(reducers, {}, applyMiddleware()); // creating a redux store
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk)); // creating a redux store
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector("#root"));
 
